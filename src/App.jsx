cb3eb6bff2b5672a0f6aa540bf3b67bfc1658087
Hamza Akhtar <hamzaakhtar8812@gmail.com>
2022-06-05 18:51:02 +0500

    Front End Skeleton Created

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <p>
+            Hello Guys! <br/>
+            This is Hamza ... <br/>
+            Reply me with your Birthdays so we can celebrate your special days with a little techy twist <br/>
+        </p>
+    </div>
+  )
+}
+
+export default App
