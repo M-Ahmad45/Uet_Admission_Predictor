1e724e303997ebb6d599ffc6a9385e4ea1e82e10
Fahad-Habib <mr.fahadhabib@gmail.com>
2022-06-22 19:12:36 +0500

    No message

--- src/component/Drawer.js
+++ src/component/Drawer.js
@@ -59,3 +59,73 @@ const classes = useStyles();
   );
 }
 export default DrawerComponent;
+
+const styles = {
+  sideNav: {
+    marginTop: '-60px',
+    zIndex: 3,
+    marginLeft: '0px',
+    position: 'fixed',
+  },
+  link: {
+    color: 'black',
+    textDecoration: 'none',
+  }
+};
+  
+export default class MarerialUIDrawer 
+    extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDrawerOpened: false,
+    };
+  }
+  toggleDrawerStatus = () => {
+    this.setState({
+      isDrawerOpened: true,
+    })
+  }
+  closeDrawer = () => {
+    this.setState({
+      isDrawerOpened: false,
+    })
+  }
+  render() {
+    const { isDrawerOpened } = this.state;
+    return (
+      <div>
+         <div style={styles.sideNav}>
+            <IconButton onClick={this.toggleDrawerStatus}>
+              {!isDrawerOpened ? <ReorderIcon /> : null }
+            </IconButton>
+          </div>
+          <Divider/>
+        <Drawer
+          variant="temporary"
+          open={isDrawerOpened}
+          onClose={this.closeDrawer}
+        >
+          <Link to='/about' style={styles.link}>
+            <List>
+              <ListItem button key='About Us'>
+                <ListItemIcon><AccountCircleIcon/>
+                </ListItemIcon>
+                <ListItemText primary='About Us' />
+              </ListItem>
+            </List>
+          </Link>
+          <Link to='/contact' style={styles.link}>
+          <List>
+            <ListItem button key='Contact Us'>
+              <ListItemIcon><PermContactCalendarIcon/>
+              </ListItemIcon>
+              <ListItemText primary='Contact Us' />
+            </ListItem>
+            </List>
+          </Link>
+        </Drawer>
+      </div>
+    );
+  }
+}
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,70 +1,111 @@
 import React from "react";
 import {
-  AppBar,
-  Toolbar,
-  CssBaseline,
-  Typography,
   makeStyles,
   useTheme,
   useMediaQuery,
 } from "@material-ui/core";
 import { Link } from "react-router-dom";
-import DrawerComponent from "./Drawer";
 
 const useStyles = makeStyles((theme) => ({
-  navlinks: {
-    marginLeft: theme.spacing(5),
-    display: "flex",
+  sideNav: {
+    marginTop: '-60px',
+    zIndex: 3,
+    marginLeft: '0px',
+    position: 'fixed',
+  },
+  navbar: {
+    overflow: "hidden",
+    width: "100%",
+    height: "100px",
+    position: "sticky",
+    backgroundColor: "#f9f9f9",
   },
   logo: {
-    flexGrow: "1",
     cursor: "pointer",
+    width: "134px",
+    height: "90px",
+    marginTop: "2px",
+    marginLeft: "5%",
+    marginRight: "40px",
+    float: "left",
   },
-  link: {
+  nav_ent: {
+    float: "right",
+    display: "block",
+    color: "#686868",
+    marginTop: "30px",
+    marginRight: "140px",
     textDecoration: "none",
-    color: "white",
-    fontSize: "20px",
-    marginLeft: theme.spacing(20),
+    fontSize: "30px",
     "&:hover": {
-      color: "yellow",
-      borderBottom: "1px solid white",
+      color: "#2668aa",
     },
+  },
+  nav_ent_mob: {
+    float: "right",
+    display: "block",
+    color: "#686868",
+    marginTop: "30px",
+    marginRight: "140px",
+    textDecoration: "none",
+    fontSize: "30px",
+    "&:hover": {
+      color: "#2668aa",
+    },
+  },
+  navbar_toggle: {
+      border: "3px solid transparent",
+      position: "fixed",
+      right: "6%",
+      top: "40px",
+      cursor: "pointer",
+  },
+  icon_bar: {
+      width: "25px",
+      backgroundColor: "#686868",
+      height: "3px",
+      margin: "5px 4px",
+      borderRadius: "5px",
   },
 }));
 
 function Navbar() {
-  const classes = useStyles();
+  const styles = useStyles();
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
+  // function toggle() {
+  //   let x = document.getElementById("navbar");
+  //   if (x.className === "navbar") {
+  //       x.className += "_toggle";
+  //   }
+  //   else {
+  //       x.className = "navbar";
+  //   }
+  // }
+
   return (
-    <AppBar position="static">
-      <CssBaseline />
-      <Toolbar>
-        <Typography variant="h4" className={classes.logo}>
-          UET Admission Predictor
-          
-        </Typography>
-        {isMobile ? (
-          <DrawerComponent />
+    <nav>
+      {isMobile ? (
+          <nav className={styles.navbar} id="navbar">
+            <div className={styles.navbar_toggle}>
+              <div className={styles.icon_bar}></div>
+              <div className={styles.icon_bar}></div>
+              <div className={styles.icon_bar}></div>
+            </div>
+          </nav>
         ) : (
-          <div className={classes.navlinks}>
-            <Link to="/" className={classes.link}>
-              Home
+          <nav className={styles.navbar} id="navbar">
+            <Link to="/" className={styles.logo}>
+              <img className={styles.logo} src={require("./logo.png")} alt="logo"/>
             </Link>
-            <Link to="/about" className={classes.link}>
-              About
-            </Link>
-            <Link to="/contact" className={classes.link}>
-              Contact
-            </Link>
-            <Link to="/faq" className={classes.link}>
-              FAQ
-            </Link>
-          </div>
-        )}
-      </Toolbar>
-    </AppBar>
+            <Link to="/faq" className={styles.nav_ent}>FAQ</Link>
+            <Link to="/contact" className={styles.nav_ent}>Contact Us</Link>
+            <Link to="/about" className={styles.nav_ent}>About</Link>
+          </nav>
+        )
+      }
+    </nav>
   );
 }
 export default Navbar;
