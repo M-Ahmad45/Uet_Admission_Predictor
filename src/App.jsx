0b8c08eb5443b33e291291a68564abeefe0ab3a7
Taimoorhasanmalik <taimoorhasanmalik@gmail.com>
2022-06-30 12:43:27 +0500

    Quote added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./component/Navbar";
-
+import Quote from "./component/Quote";
 function App() {
   return (
     <Router>
       <Navbar />
-      
+      <Quote />
     </Router>
   );
 }
