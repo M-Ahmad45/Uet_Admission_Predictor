80cbd024233a6c86b3a94768737eb407c023eb62
Hamza Akhtar <hamzaakhtar8812@gmail.com>
2022-07-21 20:33:28 +0500

    Quote Component Activated

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./component/Navbar";
 import Contact from "./component/Contact";
+import Quote from "./component/Quote";
+import Description from "./component/Description";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+    <>
+     
+      <Router>
+        <Navbar />
+        <Quote />
+        {/* <Description /> */}
+        <Routes>
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
+
 export default App;
