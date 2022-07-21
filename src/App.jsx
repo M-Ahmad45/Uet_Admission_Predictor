9d514c65439c3b1a90dac35249d7eb0c3e269374
Hamza Akhtar <hamzaakhtar8812@gmail.com>
2022-07-21 20:20:14 +0500

    App.jsx is Fixed and App is Running

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./component/Navbar";
+import Contact from "./component/Contact";
+
 function App() {
   return (
     <Router>
